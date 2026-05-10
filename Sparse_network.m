2fa34ca0acc3d36dc9c8ae975618c6b895383d7e
BinW3233 <binw417@gmail.com>
2026-05-09 23:30:46 -0400

    Fix sparse-network firing-rate output

--- Sparse_network.m
+++ Sparse_network.m
@@ -1,10 +1,10 @@
 clearvars
 
 %% Parameters for the model
-size = 600;
+nsize = 600;   % Number of neurons in the network
 sigmaw = 1;
-p = 2;
-fw = 0.3;
+p = 2;         % Number of stimulus pairs
+fw = 0.3;      % Connection probability for the sparse recurrent matrix
 theta = 0;
 tf = 6;
 mu = 0.97;
@@ -13,28 +13,27 @@ th = 7;
 pL = @(x) (x .* (x >= 0 & x < th)) + (th .* (x >= th));
 
 %% Specify the stimulus condition
-x = zeros(p,1);
-y = zeros(p,1);
+x = zeros(p, 1);
+y = zeros(p, 1);
 x(1) = 1;
 y(1) = 0;
 
 %% Sample the random weight vectors
-mulnormal = mvnrnd([0, 0], sigmaw^2*[1, sqrt(mu); sqrt(mu), 1], size*p);
-w = reshape(mulnormal(:, 1), size, p);
-v = reshape(mulnormal(:, 2), size, p);
-jmatrix0 = (w * w' + v * v') / size;
-rand_matrix = rand(size);
+mulnormal = mvnrnd([0, 0], sigmaw^2*[1, sqrt(mu); sqrt(mu), 1], nsize*p);
+w = reshape(mulnormal(:, 1), nsize, p);
+v = reshape(mulnormal(:, 2), nsize, p);
+jmatrix0 = (w * w' + v * v') / nsize;
+rand_matrix = rand(nsize);
 kij = rand_matrix < fw;
-jmatrix = jmatrix0.*kij;
+jmatrix = jmatrix0 .* kij;
 
 %% Get mismatch 1 response
-h0 = zeros(size, 1);
-dhdt = @(t, h) -h - b * jmatrix * pL(h - theta) + b * (w * x + v * y);
+h0 = zeros(nsize, 1);
+dhdt = @(t, h) -h - b * jmatrix * pL(h - theta) + b * (w*x + v*y);
 [t1, h1] = ode23tb(dhdt, [0, tf], h0);
-r1 = pL(h1); % time-dependent firing rate
+r1 = pL(h1 - theta); % Time-dependent firing rate
 
 %% Save the data
-title = 'Sparse_NN.mat';
-save(title, 'h1', 'r1');
-
-
+outputFile = 'Sparse_NN.mat';
+save(outputFile, 't1', 'h1', 'r1', 'jmatrix', 'w', 'v', ...
+    'theta', 'tf', 'mu', 'b', 'p', 'fw');
